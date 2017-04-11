5d6915653769a84e9247d83b95af3202aa0d0ab8
SG master <1363028533@qq.com>
2017-04-11 23:04:25 +0800

    Add files via upload

--- sgwl.js
+++ sgwl.js
@@ -1,4 +1,5 @@
 //Initialize the page and canvas.
+makeFont();
 sgSetup();
 
 document.getElementById("headText").textContent = Window.winName;
@@ -11,11 +12,16 @@ Screen.buffer1 = ctx.createImageData(Window.winWidth, Window.winHeight);
 Screen.buffer2 = ctx.createImageData(Window.winWidth, Window.winHeight);
 
 //Show the buffer in the page.
-var time = 0;
+var timeBegin = new Date().getTime(), timeEnd;
 setInterval(update, 0);
 function update() {
-    time++;
-    sgLoop();
+	if (!_inDelay)sgLoop();
+	//if (_sglMode == 1)textLoop();
+    timeEnd = new Date().getTime();
+    if(timeEnd - timeBegin > _vectDelta){
+    	timeBegin = new Date().getTime();
+    	Vector._v8();
+    }
     if(_visualPage==0)ctx.putImageData(Screen.buffer1, 0, 0);
     else ctx.putImageData(Screen.buffer2, 0, 0);
 }
@@ -33,8 +39,48 @@ onkeyup = function(event){
 		Key.push(e.keyCode&0x7FFF);
 	}
 }; 
-
-c.onmousemove = function(event){
-	debugger;
+onkeypress = function(event){
+	var e = event || window.event || arguments.callee.caller.arguments[0];
+	if(e){
+		_vectKey = e.keyCode;
+		Vector._v9();
+		_vectKey = 0;
+	}
 }
 
+//Response the mouse event.
+c.onmousemove = function(event){
+	var e = event || window.event || arguments.callee.caller.arguments[0];
+	if(e){
+		Mouse.pos.x = e.layerX;
+		Mouse.pos.y = e.layerY;
+		Mouse.left = e.buttons & 0x1;
+		Mouse.right = e.buttons & 0x2;
+		Mouse.middle = e.buttons & 0x4;
+	}
+}
+onmousedown = function(event){
+	var e = event || window.event || arguments.callee.caller.arguments[0];
+	if(e){
+		Mouse.left = e.buttons & 0x1;
+		Mouse.right = e.buttons & 0x2;
+		Mouse.middle = e.buttons & 0x4;
+	}
+}
+onmouseup = function(event){
+	var e = event || window.event || arguments.callee.caller.arguments[0];
+	if(e){
+		if(Mouse.left != e.buttons & 0x1){
+			Mouse.left = e.buttons & 0x1;
+			Mouse.push({ x:e.layerX, y:e.layerY, m:1});
+		}
+		if(Mouse.right != e.buttons & 0x2){
+			Mouse.right = e.buttons & 0x2;
+			Mouse.push({ x:e.layerX, y:e.layerY, m:2});
+		}
+		if(Mouse.middle != e.buttons & 0x4){
+			Mouse.middle = e.buttons & 0x4;
+			Mouse.push({ x:e.layerX, y:e.layerY, m:4});
+		}
+	}
+}
--- test.js
+++ test.js
@@ -1,13 +1,252 @@
 document.write("<script src = 'sgwl.js'></script>")
 
-function sgSetup(){
-	initWindow(640, 480, "Test succeed", 0);
+function inRect(x, y, x1, y1, x2, y2){ return (x>=x1&&x<=x2)&&(y>=y1&&y<=y2); }
+function speedUp(e){ return (e>0?2:-2); }
+
+var player = {
+	pos:{ x:0, y:0 },
+	oldImage:{}
 }
-function sgLoop(){
-    for (var i = 0; i < Screen.buffer1.data.length; i += 4) {
-        Screen.buffer1.data[i + 0] = time % 256;
-        Screen.buffer1.data[i + 1] = 0;
-        Screen.buffer1.data[i + 2] = 0;
-        Screen.buffer1.data[i + 3] = 255;
-    }
+var enemy1 = {
+	pos:{ x:0, y:0 },
+	vel:{ x:0, y:0 },
+	oldImage:{}
+}
+var enemy2 = {
+	pos:{ x:0, y:0 },
+	vel:{ x:0, y:0 },
+	oldImage:{}
+}
+var enemy3 = {
+	pos:{ x:0, y:0 },
+	vel:{ x:0, y:0 },
+	oldImage:{}
+}
+var enemy4 = {
+	pos:{ x:0, y:0 },
+	vel:{ x:0, y:0 },
+	oldImage:{}
+}
+
+var liveTime = 0, first = 1, cont = 0, drag = 0;
+
+function movePlayer(x, y) {
+	putImage(player.pos.x, player.pos.y, player.oldImage, 0);
+	setColor(255, 0, 0);
+	player.pos.x = x;
+	player.pos.y = y;
+	player.oldImage = getImage(player.pos.x, player.pos.y, player.pos.x + 83, player.pos.y + 63);
+	putQuad(player.pos.x, player.pos.y, player.pos.x + 59, player.pos.y + 59, 0);
+}
+function moveEnemy() {
+	putImage(enemy1.pos.x, enemy1.pos.y, enemy1.oldImage, 0);
+	putImage(enemy2.pos.x, enemy2.pos.y, enemy2.oldImage, 0);
+	putImage(enemy3.pos.x, enemy3.pos.y, enemy3.oldImage, 0);
+	putImage(enemy4.pos.x, enemy4.pos.y, enemy4.oldImage, 0);
+
+	if (liveTime % 10000 < 48) {
+		enemy1.vel.x += speedUp(enemy1.vel.x);
+		enemy1.vel.y += speedUp(enemy1.vel.y);
+		enemy2.vel.x += speedUp(enemy2.vel.x);
+		enemy2.vel.y += speedUp(enemy2.vel.y);
+		enemy3.vel.x += speedUp(enemy3.vel.x);
+		enemy3.vel.y += speedUp(enemy3.vel.y);
+		enemy4.vel.x += speedUp(enemy4.vel.x);
+		enemy4.vel.y += speedUp(enemy4.vel.y);
+	}
+
+	enemy1.pos.x += enemy1.vel.x;
+	enemy1.pos.y += enemy1.vel.y;
+	enemy2.pos.x += enemy2.vel.x;
+	enemy2.pos.y += enemy2.vel.y;
+	enemy3.pos.x += enemy3.vel.x;
+	enemy3.pos.y += enemy3.vel.y;
+	enemy4.pos.x += enemy4.vel.x;
+	enemy4.pos.y += enemy4.vel.y;
+
+	if (enemy1.pos.x < 0) {
+		enemy1.pos.x = -enemy1.pos.x;
+		enemy1.vel.x = -enemy1.vel.x;
+	}
+	if (enemy1.pos.x > 555) {
+		enemy1.pos.x = 1110-enemy1.pos.x;
+		enemy1.vel.x = -enemy1.vel.x;
+	}
+	if (enemy1.pos.y < 0) {
+		enemy1.pos.y = -enemy1.pos.y;
+		enemy1.vel.y = -enemy1.vel.y;
+	}
+	if (enemy1.pos.y > 575) {
+		enemy1.pos.y = 1150 - enemy1.pos.y;
+		enemy1.vel.y = -enemy1.vel.y;
+	}
+
+	if (enemy2.pos.x < 0) {
+		enemy2.pos.x = -enemy2.pos.x;
+		enemy2.vel.x = -enemy2.vel.x;
+	}
+	if (enemy2.pos.x > 503) {
+		enemy2.pos.x = 1006 - enemy2.pos.x;
+		enemy2.vel.x = -enemy2.vel.x;
+	}
+	if (enemy2.pos.y < 0) {
+		enemy2.pos.y = -enemy2.pos.y;
+		enemy2.vel.y = -enemy2.vel.y;
+	}
+	if (enemy2.pos.y > 607) {
+		enemy2.pos.y = 1214 - enemy2.pos.y;
+		enemy2.vel.y = -enemy2.vel.y;
+	}
+
+	if (enemy3.pos.x < 0) {
+		enemy3.pos.x = -enemy3.pos.x;
+		enemy3.vel.x = -enemy3.vel.x;
+	}
+	if (enemy3.pos.x > 579) {
+		enemy3.pos.x = 1158 - enemy3.pos.x;
+		enemy3.vel.x = -enemy3.vel.x;
+	}
+	if (enemy3.pos.y < 0) {
+		enemy3.pos.y = -enemy3.pos.y;
+		enemy3.vel.y = -enemy3.vel.y;
+	}
+	if (enemy3.pos.y > 543) {
+		enemy3.pos.y = 1086 - enemy3.pos.y;
+		enemy3.vel.y = -enemy3.vel.y;
+	}
+
+	if (enemy4.pos.x < 0) {
+		enemy4.pos.x = -enemy4.pos.x;
+		enemy4.vel.x = -enemy4.vel.x;
+	}
+	if (enemy4.pos.x > 575) {
+		enemy4.pos.x = 1150 - enemy4.pos.x;
+		enemy4.vel.x = -enemy4.vel.x;
+	}
+	if (enemy4.pos.y < 0) {
+		enemy4.pos.y = -enemy4.pos.y;
+		enemy4.vel.y = -enemy4.vel.y;
+	}
+	if (enemy4.pos.y > 588) {
+		enemy4.pos.y = 1176 - enemy4.pos.y;
+		enemy4.vel.y = -enemy4.vel.y;
+	}
+
+	enemy1.oldImage = getImage(enemy1.pos.x, enemy1.pos.y, enemy1.pos.x + 83, enemy1.pos.y + 63);
+	enemy2.oldImage = getImage(enemy2.pos.x, enemy2.pos.y, enemy2.pos.x + 135, enemy2.pos.y + 31);
+	enemy3.oldImage = getImage(enemy3.pos.x, enemy3.pos.y, enemy3.pos.x + 59, enemy3.pos.y + 95);
+	enemy4.oldImage = getImage(enemy4.pos.x, enemy4.pos.y, enemy4.pos.x + 63, enemy4.pos.y + 51);
+	setColor(0, 0, 255);
+	putQuad(enemy1.pos.x, enemy1.pos.y, enemy1.pos.x + 83, enemy1.pos.y + 63, 0);
+	putQuad(enemy2.pos.x, enemy2.pos.y, enemy2.pos.x + 135, enemy2.pos.y + 31, 0);
+	putQuad(enemy3.pos.x, enemy3.pos.y, enemy3.pos.x + 59, enemy3.pos.y + 95, 0);
+	putQuad(enemy4.pos.x, enemy4.pos.y, enemy4.pos.x + 63, enemy4.pos.y + 51, 0);
+}
+function hasCollision() {
+	if (inRect(player.pos.x, player.pos.y, enemy1.pos.x, enemy1.pos.y, enemy1.pos.x + 83, enemy1.pos.y + 63))return 1;
+	if (inRect(player.pos.x, player.pos.y + 29, enemy1.pos.x, enemy1.pos.y, enemy1.pos.x + 83, enemy1.pos.y + 63))return 1;
+	if (inRect(player.pos.x, player.pos.y + 59, enemy1.pos.x, enemy1.pos.y, enemy1.pos.x + 83, enemy1.pos.y + 63))return 1;
+	if (inRect(player.pos.x + 29, player.pos.y, enemy1.pos.x, enemy1.pos.y, enemy1.pos.x + 83, enemy1.pos.y + 63))return 1;
+	if (inRect(player.pos.x + 29, player.pos.y + 59, enemy1.pos.x, enemy1.pos.y, enemy1.pos.x + 83, enemy1.pos.y + 63))return 1;
+	if (inRect(player.pos.x + 59, player.pos.y, enemy1.pos.x, enemy1.pos.y, enemy1.pos.x + 83, enemy1.pos.y + 63))return 1;
+	if (inRect(player.pos.x + 59, player.pos.y + 29, enemy1.pos.x, enemy1.pos.y, enemy1.pos.x + 83, enemy1.pos.y + 63))return 1;
+	if (inRect(player.pos.x + 59, player.pos.y + 59, enemy1.pos.x, enemy1.pos.y, enemy1.pos.x + 83, enemy1.pos.y + 63))return 1;
+
+	if (inRect(player.pos.x, player.pos.y, enemy2.pos.x, enemy2.pos.y, enemy2.pos.x + 135, enemy2.pos.y + 31))return 1;
+	if (inRect(player.pos.x, player.pos.y + 29, enemy2.pos.x, enemy2.pos.y, enemy2.pos.x + 135, enemy2.pos.y + 31))return 1;
+	if (inRect(player.pos.x, player.pos.y + 59, enemy2.pos.x, enemy2.pos.y, enemy2.pos.x + 135, enemy2.pos.y + 31))return 1;
+	if (inRect(player.pos.x + 29, player.pos.y, enemy2.pos.x, enemy2.pos.y, enemy2.pos.x + 135, enemy2.pos.y + 31))return 1;
+	if (inRect(player.pos.x + 29, player.pos.y + 59, enemy2.pos.x, enemy2.pos.y, enemy2.pos.x + 135, enemy2.pos.y + 31))return 1;
+	if (inRect(player.pos.x + 59, player.pos.y, enemy2.pos.x, enemy2.pos.y, enemy2.pos.x + 135, enemy2.pos.y + 31))return 1;
+	if (inRect(player.pos.x + 59, player.pos.y + 29, enemy2.pos.x, enemy2.pos.y, enemy2.pos.x + 135, enemy2.pos.y + 31))return 1;
+	if (inRect(player.pos.x + 59, player.pos.y + 59, enemy2.pos.x, enemy2.pos.y, enemy2.pos.x + 135, enemy2.pos.y + 31))return 1;
+
+	if (inRect(player.pos.x, player.pos.y, enemy3.pos.x, enemy3.pos.y, enemy3.pos.x + 59, enemy3.pos.y + 95))return 1;
+	if (inRect(player.pos.x, player.pos.y + 29, enemy3.pos.x, enemy3.pos.y, enemy3.pos.x + 59, enemy3.pos.y + 95))return 1;
+	if (inRect(player.pos.x, player.pos.y + 59, enemy3.pos.x, enemy3.pos.y, enemy3.pos.x + 59, enemy3.pos.y + 95))return 1;
+	if (inRect(player.pos.x + 29, player.pos.y, enemy3.pos.x, enemy3.pos.y, enemy3.pos.x + 59, enemy3.pos.y + 95))return 1;
+	if (inRect(player.pos.x + 29, player.pos.y + 59, enemy3.pos.x, enemy3.pos.y, enemy3.pos.x + 59, enemy3.pos.y + 95))return 1;
+	if (inRect(player.pos.x + 59, player.pos.y, enemy3.pos.x, enemy3.pos.y, enemy3.pos.x + 59, enemy3.pos.y + 95))return 1;
+	if (inRect(player.pos.x + 59, player.pos.y + 29, enemy3.pos.x, enemy3.pos.y, enemy3.pos.x + 59, enemy3.pos.y + 95))return 1;
+	if (inRect(player.pos.x + 59, player.pos.y + 59, enemy3.pos.x, enemy3.pos.y, enemy3.pos.x + 59, enemy3.pos.y + 95))return 1;
+
+	if (inRect(player.pos.x, player.pos.y, enemy4.pos.x, enemy4.pos.y, enemy4.pos.x + 63, enemy4.pos.y + 51))return 1;
+	if (inRect(player.pos.x, player.pos.y + 29, enemy4.pos.x, enemy4.pos.y, enemy4.pos.x + 63, enemy4.pos.y + 51))return 1;
+	if (inRect(player.pos.x, player.pos.y + 59, enemy4.pos.x, enemy4.pos.y, enemy4.pos.x + 63, enemy4.pos.y + 51))return 1;
+	if (inRect(player.pos.x + 29, player.pos.y, enemy4.pos.x, enemy4.pos.y, enemy4.pos.x + 63, enemy4.pos.y + 51))return 1;
+	if (inRect(player.pos.x + 29, player.pos.y + 59, enemy4.pos.x, enemy4.pos.y, enemy4.pos.x + 63, enemy4.pos.y + 51))return 1;
+	if (inRect(player.pos.x + 59, player.pos.y, enemy4.pos.x, enemy4.pos.y, enemy4.pos.x + 63, enemy4.pos.y + 51))return 1;
+	if (inRect(player.pos.x + 59, player.pos.y + 29, enemy4.pos.x, enemy4.pos.y, enemy4.pos.x + 63, enemy4.pos.y + 51))return 1;
+	if (inRect(player.pos.x + 59, player.pos.y + 59, enemy4.pos.x, enemy4.pos.y, enemy4.pos.x + 63, enemy4.pos.y + 51))return 1;
+
+	if (!inRect(player.pos.x, player.pos.y, 40, 40, 539, 539))return 1;
+	return 0;
+}
+
+function sgSetup() {
+	initWindow(640, 640, "Collision --SGL Sample Game", 0);
+	initMouse();
+}
+function sgLoop() {
+	var position = { x:0, y:0 };
+	var press;
+
+	if (first) {
+		setColor(0, 0, 0);
+		clearScreen();
+		setColor(255, 255, 255);
+		putQuad(40, 40, 599, 599, 0);
+		player.pos.x = player.pos.y = 290;
+		enemy1.pos.x = 120;
+		enemy1.pos.y = 100;
+		enemy2.pos.x = 432;
+		enemy2.pos.y = 120;
+		enemy3.pos.x = 100;
+		enemy3.pos.y = 432;
+		enemy4.pos.x = 480;
+		enemy4.pos.y = 480;
+		enemy1.vel.x = enemy1.vel.y = enemy2.vel.y = enemy3.vel.x = 4;
+		enemy4.vel.x = enemy4.vel.y = enemy3.vel.y = enemy2.vel.x = -4;
+		player.oldImage = getImage(player.pos.x, player.pos.y, player.pos.x + 59, player.pos.y + 59);
+		enemy1.oldImage = getImage(enemy1.pos.x, enemy1.pos.y, enemy1.pos.x + 83, enemy1.pos.y + 63);
+		enemy2.oldImage = getImage(enemy2.pos.x, enemy2.pos.y, enemy2.pos.x + 135, enemy2.pos.y + 31);
+		enemy3.oldImage = getImage(enemy3.pos.x, enemy3.pos.y, enemy3.pos.x + 59, enemy3.pos.y + 95);
+		enemy4.oldImage = getImage(enemy4.pos.x, enemy4.pos.y, enemy4.pos.x + 63, enemy4.pos.y + 51);
+		setColor(255, 0, 0);
+		putQuad(player.pos.x, player.pos.y, player.pos.x + 59, player.pos.y + 59, 0);
+		setColor(0, 0, 255);
+		putQuad(enemy1.pos.x, enemy1.pos.y, enemy1.pos.x + 83, enemy1.pos.y + 63, 0);
+		putQuad(enemy2.pos.x, enemy2.pos.y, enemy2.pos.x + 135, enemy2.pos.y + 31, 0);
+		putQuad(enemy3.pos.x, enemy3.pos.y, enemy3.pos.x + 59, enemy3.pos.y + 95, 0);
+		putQuad(enemy4.pos.x, enemy4.pos.y, enemy4.pos.x + 63, enemy4.pos.y + 51, 0);
+		first = 0;
+	}
+
+	press = mouseStatus(1);
+	position = mousePos();
+
+	setColor(0, 0, 0);
+	putQuad(0, 0, 639, 39, 0);
+	setColor(255, 255, 0);
+	putNumber(liveTime, 640, 0, 'r');
+
+	if (hasCollision()) {
+		first = 1;
+		cont = 0;
+		drag = 0;
+		return;
+	}
+	if (press==1&&(inRect(position.x, position.y, player.pos.x, player.pos.y, player.pos.x+59, player.pos.y+59)||drag)) {
+		if(!cont)liveTime = 0;
+		cont = drag = 1;
+		movePlayer(position.x - 29, position.y - 29);
+	}
+	if (press == 0 && inRect(position.x, position.y, player.pos.x, player.pos.y, player.pos.x + 59, player.pos.y + 59))drag = 0;
+	if (!cont)return;
+	else {
+		moveEnemy();
+		delayEnd(40);
+		delayBegin();
+		liveTime += 48;
+	}
 }
